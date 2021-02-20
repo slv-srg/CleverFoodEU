ed97d2d19922f991d534eb8ee5400c8ef3e65787
Lev Levin <levinside@gmail.com>
2021-02-20 22:47:42 +0300

    replace wildcards

--- tokens/keys.js
+++ tokens/keys.js
@@ -1,5 +1,5 @@
 export default {
-  domain: '***.amocrm.ru',
   client_id: '__INSERT_AMOCRM_CLIENT_ID_HERE__',
   client_secret: '__INSERT_AMOCRM_CLIENT_SECRET_HERE__',
+  domain: '__MY_DOMAIN__.amocrm.ru',
 };
